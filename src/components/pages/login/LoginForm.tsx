7ebd54d0400119f1bd98142fda07568571d52975
nightingale <wb.dev03@gmail.com>
2025-04-02 10:35:47 +0800

    chore: minor refactor on login ui

--- src/components/pages/login/LoginForm.tsx
+++ src/components/pages/login/LoginForm.tsx
@@ -7,7 +7,6 @@ import { Label } from "@/components/ui/label";
 import { loginSchema } from "@/schemas/auth/login";
 import { LoginSchema } from "@/schemas/auth/login";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Anvil } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { FormError } from "@/components/ui/formError";
 
@@ -32,58 +31,38 @@ export const LoginForm = () => {
     <>
       <form onSubmit={handleSubmit(handleLogin)}>
         <div className="flex flex-col gap-6">
-          <div className="flex flex-col items-center gap-2">
-            <a
-              href="#"
-              className="flex flex-col items-center gap-2 font-medium"
-            >
-              <div className="flex size-8 items-center justify-center rounded-md">
-                <Anvil className="size-6" />
-              </div>
-            </a>
-            <h1 className="text-xl font-bold">Welcome to PDP</h1>
-            <div className="text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
+          <div className="flex flex-col gap-1">
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                {...register("email", {
+                  required: "This field is required",
+                })}
+                placeholder="hello@email.com"
+              />
             </div>
+            <FormError errors={errors} name="email" />
           </div>
-          <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-1">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  {...register("email", {
-                    required: "This field is required",
-                  })}
-                  placeholder="hello@email.com"
-                />
-              </div>
-              <FormError errors={errors} name="email" />
+          <div className="flex flex-col gap-1">
+            <div className="grid gap-3">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                {...register("password", {
+                  required: "This field is required",
+                })}
+                type="password"
+              />
             </div>
-            <div className="flex flex-col gap-1">
-              <div className="grid gap-3">
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  {...register("password", {
-                    required: "This field is required",
-                  })}
-                  type="password"
-                />
-              </div>
-              <FormError errors={errors} name="password" />
-            </div>
-
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={isSubmitting}
-              isLoading={isSubmitting}
-            >
-              Login
-            </Button>
+            <FormError errors={errors} name="password" />
           </div>
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={isSubmitting}
+            isLoading={isSubmitting}
+          >
+            Login
+          </Button>
         </div>
       </form>
     </>
--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -1,10 +1,30 @@
+import { Anvil } from "lucide-react";
 import { LoginForm } from "./LoginForm";
 
 export const LoginIndex = () => {
   return (
     <div className="w-full max-w-sm p-6 md:p-10">
       <div className="flex flex-col gap-6">
-        <LoginForm />
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <a
+              href="#"
+              className="flex flex-col items-center gap-2 font-medium"
+            >
+              <div className="flex size-8 items-center justify-center rounded-md">
+                <Anvil className="size-6" />
+              </div>
+            </a>
+            <h1 className="text-xl font-bold">Welcome to PDP</h1>
+            <div className="text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </div>
+          <LoginForm />
+        </div>
         <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
           By clicking continue, you agree to our Terms of Service and Privacy
           Policy.
