fcc27f6279ea21bd95557e10d8bfaf617ffa3410
nightingale <wb.dev03@gmail.com>
2025-04-24 17:02:38 +0800

    chore: revert prisma adapter changes

--- packages/platform/src/lib/prisma.ts
+++ packages/platform/src/lib/prisma.ts
@@ -5,7 +5,7 @@ import { Pool, neonConfig } from "@neondatabase/serverless";
 import ws from "ws";
 import { PrismaClient } from "@prisma/client";
 
-// neonConfig.webSocketConstructor = ws;
+neonConfig.webSocketConstructor = ws;
 
 // To work in edge enviro nments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true;
@@ -16,11 +16,11 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-// const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-// const pool = new Pool({ connectionString });
-// const adapter = new PrismaNeon(pool);
-const prisma = global.prisma || new PrismaClient();
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
